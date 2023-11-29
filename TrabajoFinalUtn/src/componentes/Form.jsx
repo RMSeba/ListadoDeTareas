d94e44e6028e39a366f8b134d564351dfa43e4ee
RMSeba <Romeromartin.98@hotmail.com>
2023-11-29 14:34:26 -0300

    mensaje-error

--- TrabajoFinalUtn/src/componentes/Form.jsx
+++ TrabajoFinalUtn/src/componentes/Form.jsx
@@ -1,16 +1,30 @@
 
 
+import { useState } from 'react'
 import '../style.css'
 
 export const Formulario = ({cosas,setCosas}) => {
     
-    
+  const [error,setError]=useState(false)
+  const avisarError=()=>{
+      if(error){
+        return <p className='error-mensaje'>Ingrese una tarea</p>
+
+      }
+    }
     
     const agregar = (event) => {
         event.preventDefault();
         console.log(event.target.tar.value);
-        const nuevatarea={tarea:event.target.tar.value,estado:false,id:cosas ? cosas.length + 1 : 1} 
-        setCosas([...cosas ,nuevatarea])
+        if(event.target.tar.value===""){
+          setError(true)
+        }
+        else{
+          const nuevatarea={tarea:event.target.tar.value,estado:false,id:cosas ? cosas.length + 1 : 1} 
+          setCosas([...cosas ,nuevatarea])
+          setError(false)
+
+        }
         
         console.log(cosas)
         event.stopPropagation();
@@ -18,9 +32,16 @@ export const Formulario = ({cosas,setCosas}) => {
       }
     
      return (
+      <>
     <form onSubmit={agregar}>
                 <input type="text" name='tar' placeholder='Ingrese una tarea'/>
                 <button type='submit' className='btn-agregar' >Agregar tarea</button>
     </form>
+      <div className='error'>
+        {avisarError()}
+      </div>
+      
+      </>
+    
   )
 }
