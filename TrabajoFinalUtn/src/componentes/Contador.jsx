992ef8e6e290be341d5a38141aa3f0834b8fbcbe
RMSeba <Romeromartin.98@hotmail.com>
2023-11-29 00:30:09 -0300

    .

--- TrabajoFinalUtn/src/componentes/Contador.jsx
+++ TrabajoFinalUtn/src/componentes/Contador.jsx
@@ -5,25 +5,25 @@ import '../style.css'
 export const ContadorTareas = ({cosas}) => {
     
     const[contador,setContador]=useState(0);
-    
-    
-      const[realizada,setRealizada]=useState(0);
-      useEffect(() => {
+    const[realizada,setRealizada]=useState(0);
+
+    useEffect(() => {
           setContador(cosas.length);
           let tareaRealizada=0;
           let tareaPendiente=cosas.length;
-          cosas.forEach(cosa=> {
+            cosas.forEach(cosa=> {
+              
+              if(cosa.estado===true  ){
+                tareaRealizada+=1;
+                tareaPendiente-=1;
 
-            if(cosa.estado===true  ){
-              tareaRealizada+=1;
-              tareaPendiente-=1;
             }
             
          }); 
           
 
-         setRealizada(tareaRealizada)
-         setContador(tareaPendiente)
+    setRealizada(tareaRealizada)
+    setContador(tareaPendiente)
         
         
       }, [cosas]);
--- TrabajoFinalUtn/src/componentes/Form.jsx
+++ TrabajoFinalUtn/src/componentes/Form.jsx
@@ -4,11 +4,10 @@ import '../style.css'
 
 export const Formulario = ({cosas,setCosas}) => {
     
-    // const {metas,setMetas}=Funtarea();
-    // const [cosas,setCosas] = useState(metas);
+    
     
     const agregar = (event) => {
-      event.preventDefault();
+        event.preventDefault();
         console.log(event.target.tar.value);
         const nuevatarea={tarea:event.target.tar.value,estado:false,id:cosas ? cosas.length + 1 : 1} 
         setCosas([...cosas ,nuevatarea])
